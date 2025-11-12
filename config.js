b3551c0b9cb7600a049b75371288826a686c3d6c
mohammed68194-beep <mohammed68194@gmail.com>
2025-11-12 14:16:59 +0300

    البوت

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ module.exports = {
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '1435043068955988149',         // ايدي السيرفر
+        broadcastRoleId: '1435043424251285666', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1438108498641551490' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
@@ -34,4 +34,5 @@ module.exports = {
         error: '#ED4245',      // Red (used for errors)
         neutral: '#5D5D5D'     // Gray (used for neutral messages)
     }
+
 };
